063c5200bf358c89a3ce665c0a1366575169ec7c
akashsinha-43 <akashsinha432003@gmail.com>
2022-09-06 14:14:05 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,17 @@
+
+public class NewSort {
+	public static void main(String[] args) {
+		int arr[] = {6, 9 , 10, 1, 3 ,5, 7};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int temp = 0;
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
